89100d04ae20b280b5d6dc186c5970c2477e910e
palmdrop <anton@exlex.se>
2020-12-30 16:34:59 +0100

    too many changes to write sensible commit message

---
+++ .config/bspwm-polybar/scripts/layout.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+layout=$(bsp-layout get $(bspc query -D -d --names))
+
+case $layout in
+    -|tiled)
+        symbol=
+        ;;
+    monocle)
+        symbol=
+        ;;
+    even)
+        symbol=E
+        ;;
+    grid)
+        symbol=
+        ;;
+    rgrid)
+        symbol=
+        ;;
+    rtall)
+        symbol=!
+        ;;
+    rwide)
+        symbol=!
+        ;;
+    tall)
+        symbol=
+        ;;
+    wide)
+        symbol=
+        ;;
+    *)
+        symbol=""
+        ;;
+esac
+
+if [ "$layout" = "-" ] || [ "$layout" = "tiled" ]; then
+    cl=$(xrdbvar bar.ramp2)
+else 
+    cl=$(xrdbvar bar.alert)
+fi
+
+echo "%{B$cl} $symbol %{B-}"
---
+++ .config/bspwm-polybar/scripts/volbar.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Get volume level
+v=$(vol print)
+
+# background
+bg=$(xrdbvar bar.background)
+
+# Color for empty step
+blank=$(xrdbvar bar.ramp2)
+# Mute color
+mute=$(xrdbvar bar.foreground)
+# Color if volume is above 100%
+high=$(xrdbvar bar.alert)
+# Volume step color
+step=$(xrdbvar bar.ramp4)
+
+# If muted, replace step with mute color
+if (( $v > 100 )); then
+    step=$high
+elif [ "$(vol muted)" = "true" ]; then
+    step=$mute
+fi
+
+# Define variables
+fs=$(( $v/10 ))
+if (( $fs > 10 )); then
+    fs=10
+fi
+
+es=$(( 10 - fs ))
+
+filled_steps=$(printf "%${fs}s" | sed 's/ /|/g')
+empty_steps=$(printf "%${es}s" | sed 's/ /|/g')
+
+echo -e "%{B$bg}%{F$step}  $filled_steps%{B-F-}%{B$bg}%{F$blank}$empty_steps  %{B-F-}"
