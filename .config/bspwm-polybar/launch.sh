368ddae6f5ba3407175d2e0d7b26f2229b1a0e08
palmdrop <anton@exlex.se>
2021-01-03 22:04:41 +0100

    removed some unnecessary stuff

--- .config/bspwm-polybar/launch.sh
+++ .config/bspwm-polybar/launch.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+# Kill all current polybar instances
 killall -q polybar
+
+# Wait for all polybar instances to terminate
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-rm /tmp/polybar.pids
-
+# Launch main and secondary bar
 polybar -c ~/.config/bspwm-polybar/config.ini main &
-echo "$!" >> /tmp/polybar.pids
 polybar -c ~/.config/bspwm-polybar/config.ini secondary &
-echo "$!" >> /tmp/polybar.pids
-
