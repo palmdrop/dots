7ce777bcdf03b63d607d5b3fe13590ca1b3ab3a1
palmdrop <anton@exlex.se>
2021-01-13 13:35:05 +0100

    etc......

--- .config/rofi/launchers/text/launcher.sh
+++ .config/rofi/launchers/text/launcher.sh
@@ -17,13 +17,13 @@ styles=($(/usr/bin/ls -p --hide="colors.rasi" $dir/styles))
 color="${styles[$(( $RANDOM % 10 ))]}"
 
 # comment this line to disable random colors
-sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
+#sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
 
 # comment these lines to disable random style
 #themes=($(/usr/bin/ls -p --hide="launcher.sh" --hide="styles" $dir))
 #theme="${themes[$(( $RANDOM % 7 ))]}"
 
-echo $theme
+#echo $theme
 
 rofi -no-lazy-grab -show drun \
 -modi run,drun,window \
